b29562df53f3fbe86e3e2243779311a0dfdf8143
Mert Bünül <bunulmertt@hotmail.com>
2022-02-20 22:08:23 +0300

    first commit

---
+++ Inheritances/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Inheritance;
+
+
+
+namespace Inheritances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           TohumluBitkiler tohumlubitki = new TohumluBitkiler();
+           tohumlubitki.TohumlaCogalma();
+
+           Kuslar marti = new Kuslar();
+           marti.Ucmak();
+           
+
+
+        }
+    }
+}
---
+++ Siniflar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Siniflar // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisanlar calisan = new Calisanlar();
+            calisan.Ad = "Alperen";
+            calisan.Soyad = "FAKI";
+            calisan.No = 11;
+            calisan.Departman = "IT";
+            calisan.CalisanBilgileri();
+            Console.WriteLine("*********");
+            calisan.Ad = "Merve";
+            calisan.Soyad = "Çakır";
+            calisan.No = 33;
+            calisan.Departman = "Satış";
+            calisan.CalisanBilgileri();
+        }
+    }
+
+    class Calisanlar
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı : {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı : {0}", Soyad);
+            Console.WriteLine("Çalışan Numarası : {0}", No);
+            Console.WriteLine("Çalışan Departmanı : {0}", Departman);
+
+        }
+
+
+
+    }
+}
