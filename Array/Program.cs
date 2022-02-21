91e6fd04663755feb18a8eafadc46eefdaf76603
Mert Bünül <bunulmertt@hotmail.com>
2022-02-21 17:35:12 +0300

    Arrays Edit

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,29 +6,34 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            int[] a = { 7, 4, 5, 89, 34, 67, 45 };
-            foreach (var item in a)
+            int[] sayiDizisi = { 7, 4, 5, 89, 34, 67, 45 };
+            Console.WriteLine("---^^^^^^-----Unsort---------");
+            foreach (var item in sayiDizisi)
                 Console.WriteLine(item);
-            Console.WriteLine("--------Unsort---------");
-            Array.Sort(a);
-            foreach (var item in a)
+
+            Console.WriteLine("---^^^^^^------Sort-------");
+            Array.Sort(sayiDizisi);
+            foreach (var item in sayiDizisi)
                 Console.WriteLine(item);
-            Console.WriteLine("---------Sort-------");
-            Array.Clear(a, 3, 1);
-            foreach (var item in a)
+
+            Console.WriteLine("--^^^^^^-------Clear--------");
+            Array.Clear(sayiDizisi, 3, 1);
+            foreach (var item in sayiDizisi)
                 Console.WriteLine(item);
-            Console.WriteLine("---------Clear--------");
-            Array.Reverse(a);
-            foreach (var item in a)
+
+            Console.WriteLine("---^^^^^^-------Reverse------");
+            Array.Reverse(sayiDizisi);
+            foreach (var item in sayiDizisi)
                 Console.WriteLine(item);
-            Console.WriteLine("----------Reverse------");
-            Console.WriteLine(Array.IndexOf(a, 67));
-            Console.WriteLine("-----------IndexOf-------");
-            Array.Resize<int>(ref a,8);
-            a[7]= 13;
-            foreach (var item in a)
+
+            Console.WriteLine("----^^^^^^-------IndexOf-------");
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 67));
+
+            Console.WriteLine("---^^^^^^-------Resize-------");
+            Array.Resize<int>(ref sayiDizisi,8);
+            sayiDizisi[7]= 13;
+            foreach (var item in sayiDizisi)
             Console.WriteLine(item);
-            Console.WriteLine("-----------Resize-------");
 
         }
     }
