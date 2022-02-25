4b8039ef3b23925240188acebf846c8c031ac1e8
Mert Bunul <77413387+Mrbunul@users.noreply.github.com>
2022-02-25 20:43:50 +0300

    Update Program.cs

--- Hazir_Methodlar/Program.cs
+++ Hazir_Methodlar/Program.cs
@@ -6,8 +6,8 @@ namespace Hazir_Methodlar // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            string degiskenler = "Ahmet Alperen Fakı";
-            string degiskenler2 = "Ahmet Alperen";
+            string degiskenler = "Mert Deniz Bünül";
+            string degiskenler2 = "Mert Bünül";
             string[] str =degiskenler.Split(' ');
             
 
@@ -19,16 +19,16 @@ namespace Hazir_Methodlar // Note: actual namespace depends on the project name.
             Console.WriteLine(String.Compare(degiskenler,degiskenler2,true));
             Console.WriteLine(String.Compare(degiskenler,degiskenler2,false));
             Console.WriteLine(degiskenler.Contains(degiskenler2));
-            Console.WriteLine(degiskenler.EndsWith("Fakı"));
-            Console.WriteLine(degiskenler.StartsWith("Ahmet"));
-            Console.WriteLine(degiskenler.IndexOf("Alperen"));
+            Console.WriteLine(degiskenler.EndsWith("Bünül"));
+            Console.WriteLine(degiskenler.StartsWith("Mert"));
+            Console.WriteLine(degiskenler.IndexOf("Deniz"));
             Console.WriteLine(degiskenler.Insert(0,"Selam "));
             Console.WriteLine(degiskenler.LastIndexOf("k"));
             Console.WriteLine(degiskenler + degiskenler2.PadLeft(20));
             Console.WriteLine(degiskenler + degiskenler2.PadRight(20));
             Console.WriteLine(degiskenler.Remove(10));
             Console.WriteLine(degiskenler.Remove(5,3));
-            Console.WriteLine(degiskenler.Replace("Ahmet","Alperen"));
+            Console.WriteLine(degiskenler.Replace("Mert","Deniz"));
             Console.WriteLine(degiskenler.Replace(" ",""));
             Console.WriteLine(degiskenler.Split(' ')[1]);
             Console.WriteLine(degiskenler.Substring(3));
